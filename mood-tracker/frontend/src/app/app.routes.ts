226921a1cec115f639f1442f8c08c2e95d389694
Jim Tupper <gentoo@tupper.org.uk>
2025-04-17 18:15:09 +0100

    Add empty moods component

--- mood-tracker/frontend/src/app/app.routes.ts
+++ mood-tracker/frontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login.component';
+import { ListComponent } from './mood/list.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'moods', component: ListComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
